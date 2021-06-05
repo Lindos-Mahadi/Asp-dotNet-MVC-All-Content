fb56f28f6c5cf0d3c5ff951f2cc6a602c800fe71
Lindos-Mahadi <lindosmahadi@.com>
2021-06-05 17:39:34 +0600

    MVC Inital Project

---
+++ WebApplication-MVC-All-Topic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication_MVC_All_Topic.Startup))]
+namespace WebApplication_MVC_All_Topic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
