9c3e7c12ff993faadc50c1e2dcba2fd8fd1fce79
Lindos-Mahadi <lindosmahadi@.com>
2021-06-05 18:59:56 +0600

    Created Employee Model

---
+++ WebApplication-MVC-All-Topic/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_MVC_All_Topic.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Accournt { get; set; }
+
+    }
+}
